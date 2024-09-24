a330d005947af9dafa27772427175f3e16c36998
yeatasim <yeatasim.cse9.bu@gmail.com>
2024-09-24 12:27:13 +0600

    added Toggle between Donation and History section

--- javascript/index.js
+++ javascript/index.js
@@ -9,3 +9,29 @@ document.addEventListener("DOMContentLoaded", function () {
   adjustButtonTabsPosition();
   window.addEventListener("resize", adjustButtonTabsPosition);
 });
+
+// Global section
+
+const donationTab = document.getElementById("donationTab");
+const historyTab = document.getElementById("historyTab");
+const donationSection = document.getElementById("donationSection");
+const historySection = document.getElementById("historySection");
+
+// Toggle section
+function toggleTab(activeTab, inactiveTab, showSection, hideSection) {
+  activeTab.classList.add("bg-green-500", "text-white");
+  activeTab.classList.remove("bg-white", "text-gray-700", "border");
+  inactiveTab.classList.remove("bg-green-500", "text-white");
+  inactiveTab.classList.add("bg-white", "text-gray-700", "border");
+  showSection.classList.remove("hidden");
+  hideSection.classList.add("hidden");
+}
+
+// Toggle between Donation and History
+donationTab.addEventListener("click", function () {
+  toggleTab(donationTab, historyTab, donationSection, historySection);
+});
+
+historyTab.addEventListener("click", function () {
+  toggleTab(historyTab, donationTab, historySection, donationSection);
+});
